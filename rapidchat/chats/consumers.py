62bea6b48dd603a26b2f94fda0c588071196ed43
David Puerta Martín <puertadavid96182@gmail.com>
2023-10-30 14:53:43 -0400

    show when users are typing

--- rapidchat/chats/consumers.py
+++ rapidchat/chats/consumers.py
@@ -104,6 +104,15 @@ class ChatConsumer(JsonWebsocketConsumer):
                     "message": MessageSerializer(message).data,
                 },
             )
+        if message_type == "typing":
+            async_to_sync(self.channel_layer.group_send)(
+                self.conversation_name,
+                {
+                    "type": "typing",
+                    "user": self.user.username,
+                    "typing": content["typing"],
+                },
+            )
         return super().receive_json(content, **kwargs)
 
     def chat_message_echo(self, event):
@@ -120,4 +129,7 @@ class ChatConsumer(JsonWebsocketConsumer):
         self.send_json(event)
 
     def user_leave(self, event):
+        self.send_json(event)
+
+    def typing(self, event):
         self.send_json(event)
