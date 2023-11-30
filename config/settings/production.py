69e306ee257235e8e4adbc84506eb5c70f94b2a4
David Puerta Martín <puertadavid96182@gmail.com>
2023-11-29 22:34:41 -0500

    update production email settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -63,18 +63,11 @@ STORAGES = {
 
 # EMAIL
 # ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
-DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL",
-    default="rapidchat <noreply@localhost:8000>",
-)
-# https://docs.djangoproject.com/en/dev/ref/settings/#server-email
-SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = env(
-    "DJANGO_EMAIL_SUBJECT_PREFIX",
-    default="[rapidchat] ",
-)
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_USE_TLS = int(env("EMAIL_USE_TLS"))
 
 # ADMIN
 # ------------------------------------------------------------------------------
